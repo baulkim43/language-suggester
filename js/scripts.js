8d413a0fa368ac3160686a07ac742e660f62d86c
Baul Kim <baul.kim43@gmail.com>
2019-10-25 10:07:27 -0700

    add changes to input form and define variables for form.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var devices = parseInt($("input#devices").val());
+    var pants = parseInt($("input#pants").val());
+    var animal = $("select#animal").val());
+    var food = $("select#food").val());
+
+
+  }
+}
