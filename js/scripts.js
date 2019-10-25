639d75da174137f9ffaa5baefd5d26ba14a96b1f
Baul Kim <baul.kim43@gmail.com>
2019-10-25 10:38:59 -0700

    add functions and variables in js file.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var devices = parseInt($("input#devices").val());
     var pants = parseInt($("input#pants").val());
-    var animal = $("select#animal").val());
-    var food = $("select#food").val());
+    var animal = $("select#animal").val();
+    var food = $("select#food").val();
 
+    if (age <= 15) {
+      var match = "Ruby";
+    } else if 
 
-  }
-}
+    $("#code").text(match);
+    $("#match").show()
+
+    event.preventDefault();
+  });
+});
